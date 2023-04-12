41f2a2ed376c96ea2cf5d878e2b7d15a4b4cef7f
sstamoulis <sstamoulis2010@gmail.com>
2023-04-12 13:25:40 +0000

    add explanation about mapping format

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,18 @@ use mappings::{Mapping, Mappings};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
+#[command(after_long_help = "\
+Mappings can be provided in the following format:
+
+    CT_ID_START[-CT_ID_END][:HOST_ID_START[-HOST_ID_END]]
+
+CT_ID_START and CT_ID_END are the UID/GID range start and end from the container's view.
+If CT_ID_END is not provided it defaults to the same value as CT_ID_START.
+HOST_ID_START and HOST_ID_END are the UID/GID range start and end from the host's view.
+If HOST_ID_END is not provided it defaults to HOST_ID_START + CT_ID_END - CT_ID_START
+
+All range bounds are inclusive.
+")]
 struct Cli {
     #[arg(short = 'u', long = "uid")]
     /// Mappings only for uid
